84119a98607280e7f00940f40dc7c1233c29d972
salyaa <82571736+salyaa@users.noreply.github.com>
2023-11-16 12:41:13 +0100

    Update plot_helpers.py

--- plot_helpers.py
+++ plot_helpers.py
@@ -4,13 +4,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def plot_most_frequent_articles(data: pd.Series, type: str, pct: float=10, key_word: str="United_Kingdom"):
+def plot_most_frequent_articles(data: pd.Series, type: str, pct: float=10, main_article: str="United_Kingdom"):
     """ 
-    plot most frequent articles that precede/follow keyword
+    plot most frequent articles that precede/follow main_article
     input:
         data: percentage of apparition of articles
         type: precede/follow
-        key_word: key_word for selection of articles
+        main_article: main_article for selection of articles
         pct: percentage of most frequent articles to display
     """
     most_frequent = data[data >= np.percentile(data, 100 - pct)]
@@ -22,6 +22,6 @@ def plot_most_frequent_articles(data: pd.Series, type: str, pct: float=10, key_w
     plt.yscale("log")
     plt.xlabel("most frequent articles")
     plt.ylabel("%")
-    plt.title(f"Articles that {type} '{key_word}'")
+    plt.title(f"Articles that {type} '{main_article}'")
     plt.rc('xtick', labelsize=8)
     plt.xticks(rotation=90)  # Rotate x-axis labels for readability
